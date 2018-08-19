6ddb44c2c224a8f9a7d2b0d5b57ca69392965968
Mohammad Pourcheriki <Pourcheriki@gmail.com>
2018-08-20 02:09:43 +0430

    Add ClassRoomStudentListAPI

--- school/views.py
+++ school/views.py
@@ -4,12 +4,22 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from school.models import Mark
-from school.serializers import MarkSerializer
+from school.models import Mark, Student
+from school.serializers import MarkSerializer, StudentSerializer
 
 
 class StudentMarkAPI(APIView):
+
     def get(self, request, nid):
         marks = Mark.objects.filter(student__nid=nid)
         serializer = MarkSerializer(marks, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ClassRoomStudentListAPI(APIView):
+
+    def get (self, request, name, year):
+        students = Student.objects.filter(classroom__class_name=name, students__semester__year=year)
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
