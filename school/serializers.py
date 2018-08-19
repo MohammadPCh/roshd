03348f0cd4339a4fed56847be0b547d3a790328a
Mohammad Pourcheriki <Pourcheriki@gmail.com>
2018-08-20 01:27:14 +0430

    Fix typo

--- school/serializers.py
+++ school/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from school.models import Mark
 
 
-class MarksSerializer(serializers.ModelSerilizer):
+class MarksSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mark
         fields = '__all__'
