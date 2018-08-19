dd8390efb4fd41571a75e6a5be65a5dd95cd6a3e
Mohammad Pourcheriki <Pourcheriki@gmail.com>
2018-08-20 00:26:44 +0430

    Add Semester model

--- school/models.py
+++ school/models.py
@@ -1,3 +1,12 @@
+from datetime import datetime
 from django.db import models
 
+
 # Create your models here.
+class Semester(models.Model):
+    SEMESTER_CHOICES = (
+        ('1', 'Term 1'),
+        ('2', 'Term 2'),
+    )
+    semester = models.CharField(choices=SEMESTER_CHOICES, max_length=1, default=0)
+    year = models.CharField(max_length=4, default=datetime.now().year)
