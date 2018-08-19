f542e32216568ddf5f2e7485994021c62834704d
Mohammad Pourcheriki <Pourcheriki@gmail.com>
2018-08-20 01:57:11 +0430

    Fix MarkSerializer to show name instead of id

--- school/serializers.py
+++ school/serializers.py
@@ -3,6 +3,10 @@ from school.models import Mark
 
 
 class MarkSerializer(serializers.ModelSerializer):
+    student = serializers.StringRelatedField()
+    course = serializers.StringRelatedField()
+    semester = serializers.StringRelatedField()
+
     class Meta:
         model = Mark
         fields = '__all__'
