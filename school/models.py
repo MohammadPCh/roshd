8506126d486f9af849463946057e12c7139f4730
Mohammad Pourcheriki <Pourcheriki@gmail.com>
2018-08-20 00:29:10 +0430

    Add Student model

--- school/models.py
+++ school/models.py
@@ -10,3 +10,12 @@ class Semester(models.Model):
     )
     semester = models.CharField(choices=SEMESTER_CHOICES, max_length=1, default=0)
     year = models.CharField(max_length=4, default=datetime.now().year)
+
+
+class Student(models.Model):
+    # TODO add validators
+    nid = models.CharField(max_length=10, default='')
+    first_name = models.CharField(max_length=20, default='')
+    last_name = models.CharField(max_length=20, null=True)
+    father_name = models.CharField(max_length=20)
+    address = models.TextField(max_length=100, null=True, blank=True)
