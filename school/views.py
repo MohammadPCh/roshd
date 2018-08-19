b48bcc00d30796dc8cfab477b1fc4145bce47871
Mohammad Pourcheriki <Pourcheriki@gmail.com>
2018-08-20 01:58:19 +0430

    Add StudentMarkAPI view

--- school/views.py
+++ school/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from school.models import Mark
+from school.serializers import MarkSerializer
+
+
+class StudentMarkAPI(APIView):
+    def get(self, request, nid):
+        marks = Mark.objects.filter(student__nid=nid)
+        serializer = MarkSerializer(marks, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
