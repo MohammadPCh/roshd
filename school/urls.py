a3b9b54905caf17dcf6b2dfb05d3a4752828df31
Mohammad Pourcheriki <Pourcheriki@gmail.com>
2018-08-20 02:10:18 +0430

    Add ClassRoomStudentList url

--- school/urls.py
+++ school/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from school.views import StudentMarkAPI
+from school.views import StudentMarkAPI, ClassRoomStudentListAPI
 
 urlpatterns = [
-    path('API/marks/<int:nid>/', StudentMarkAPI.as_view())
+    path('API/marks/<int:nid>/', StudentMarkAPI.as_view()),
+    path('API/students/<str:name>/<int:year>/', ClassRoomStudentListAPI.as_view()),
 ]
