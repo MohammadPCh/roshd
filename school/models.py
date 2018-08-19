e9b723a0c5b65c5a620b7bf8c153e0d62f8418f2
Mohammad Pourcheriki <Pourcheriki@gmail.com>
2018-08-20 00:50:18 +0430

    Add str function to Student class

--- school/models.py
+++ school/models.py
@@ -28,6 +28,9 @@ class Student(models.Model):
     father_name = models.CharField(max_length=20)
     address = models.TextField(max_length=100, null=True, blank=True)
 
+    def __str__(self):
+        return '{0} {1}'.format(self.first_name, self.last_name)
+
 
 class Course(models.Model):
     name = models.CharField(max_length=20)
