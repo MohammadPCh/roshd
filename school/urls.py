abc4534e5ebcdbdad622af4e374160a5d88fe201
Mohammad Pourcheriki <Pourcheriki@gmail.com>
2018-08-21 21:19:02 +0430

    Change nid from int to str

--- school/urls.py
+++ school/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from school.views import *
 
 urlpatterns = [
-    path('API/marks/<int:nid>/', StudentMarkAPI.as_view()),
+    path('API/marks/<str:nid>/', StudentMarkAPI.as_view()),
     path('API/students/<str:name>/<int:year>/', ClassRoomStudentListAPI.as_view()),
     path('API/courses/average/<str:name>/<int:year>/', CourseAverageAPI.as_view()),
 ]
